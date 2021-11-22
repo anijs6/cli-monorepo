c85d426d2bfd59d17687cd305f0d4dfcbcfcccba
anijs6 <anijs666666@hotmail.com>
2021-11-22 17:54:56 +0800

    feat(*): update config

--- packages/webpack-config/webpack-common-config/webpack.common.ts
+++ packages/webpack-config/webpack-common-config/webpack.common.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-use-before-define */
 import type { Configuration } from 'webpack'
+import { autoWebpackEntryFile } from '@anijs/cli-shared-utils'
 
 /**
  * @param options 配置项
@@ -8,11 +9,24 @@ import type { Configuration } from 'webpack'
 function getCommonConfig(options: CommonConfigOptions): Configuration {
   const { mode } = options || {}
 
-  const entry = options.entryConfig ||
+  const entry = options.entryConfig || formateEntry(autoWebpackEntryFile())
   return {
     context: process.cwd(),
     mode,
-    target: mode === 'development' ? 'web' : 'browserslist'
+    target: mode === 'development' ? 'web' : 'browserslist',
+    entry
+  }
+}
+
+/**
+ * 格式化webpack entry配置
+ *
+ * @param entryFile 入口文件
+ * @returns Entry标准配置
+ */
+function formateEntry(entryFile: string): Configuration['entry'] {
+  return {
+    main: entryFile
   }
 }
 
